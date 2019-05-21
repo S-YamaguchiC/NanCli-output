029c965284ed96f22bf9898de99c02ac2d20bc72
S-YamaguchiC <cist.b216.s.yamaguchi@gmail.com>
2019-05-21 20:57:15 +0900

    決定版?

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,56 @@
 <template>
-  <div id="app">
-    <!--<img alt="Vue logo" src="./assets/logo.png">-->
-    <img alt="Vue logo" src="./assets/clickerPNG.png">
-    <HelloWorld msg="ほらクリックするんだよ"/>
-  </div>
+    <div id="app">
+        <!--<img alt="Vue logo" src="./assets/logo.png">-->
+        <img alt="Vue logo" src="./assets/clickerPNG.png">
+        <HelloWorld msg="ほらクリックするんだよ"/>
+
+        <!--TwitterのIDをさらしていくすたいる-->
+        <div class="wrapper">
+            <footer>
+                © 2019 S-YamaguchiC( <a href="https://twitter.com/kawasaki_dst" style="color: black">@kawasaki_dst</a> )
+            </footer>
+        </div>
+    </div>
 </template>
 
 
 <script>
-import HelloWorld from './components/HomePage.vue'
+    import HelloWorld from './components/HomePage.vue'
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        name: 'app',
+        components: {
+            HelloWorld
+        }
+    }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 10px;
-}
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 10px;
+    }
+
+    .wrapper{
+        height: 100%;
+        min-height: 100vh;
+        position: relative;/*←相対位置*/
+        padding-bottom: 100px;/*←footerの高さ*/
+        box-sizing: border-box;/*←全て含めてmin-height:100vhに*/
+    }
+
+    footer{
+        width: 100%;
+        background-color: #87cefa;
+        color: #fff;
+        text-align: center;
+        padding: 10px 0;
+
+        position: absolute;/*←絶対位置*/
+        bottom: 0; /*下に固定*/
+    }
 </style>
--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,23 +1,12 @@
 <template>
     <div class="hello">
-        <h1>{{ msg }}</h1>
 
         <div class="small">
 
             <h3>↓結果だお↓</h3>
             <line-chart :chart-data="datacollection" style="width: 350px;"></line-chart>
-        </div>
 
-        <p>リセットボタン</p>
-        <form id="reset_form">
-            <input type="button" @click="reset" value="チャートを初期化"/>
-        </form>
-
-        <!--TwitterのIDをさらしていくすたいる-->
-        <div class="wrapper">
-            <footer>
-                © 2019 S-YamaguchiC( <a href="https://twitter.com/kawasaki_dst" style="color: black">@kawasaki_dst</a> )
-            </footer>
+            <input type="button" @click="fillData()" value="更新ボタン">
         </div>
 
     </div>
@@ -35,16 +24,16 @@
         data () {
             return {
                 // 初期値
-                counterA: 1,
-                counterB: 1,
-                counterC: 1,
-                counterD: 1,
-                datacollection: null
+                counterA: 0,
+                counterB: 0,
+                counterC: 0,
+                counterD: 0,
+                datacollection: {}, // ここをnullにすると、console上でエラーになる
+                // object: null
             }
         },
         mounted () {
             this.fillData()
-            this.reDraw()
         },
 
         // 以下で関数を定義
@@ -52,49 +41,32 @@
 
             // チャート描画用
             fillData () {
-
                 // axiosでとってくる
-                axios.get('CGIのパス')
+                axios.post('ここにURL')
                     .then(response => {
-                        this.counterA = response.data.counterA
-                        this.counterB = "cgiのでーた"
-                        this.counterC = "cgiのでーた"
-                        this.counterD = "cgiのでーた"
+                        console.log(JSON.parse(response.status))
+                        this.counterA = JSON.stringify(response.data.vote['1'])
+                        this.counterB = JSON.stringify(response.data.vote['2'])
+                        this.counterC = JSON.stringify(response.data.vote['3'])
+                        this.counterD = JSON.stringify(response.data.vote['4'])
+                        console.log(this.counterA, this.counterB, this.counterC, this.counterD)
                     })
                     .catch(err => {
-                        this.counterA = err
-                        this.counterB = "cgiのでーた"
-                        this.counterC = "cgiのでーた"
-                        this.counterD = "cgiのでーた"
+                        var object = JSON.parse(err.status)
+                        console.log(object)
                     });
 
                 this.datacollection = {
-                    labels: ['A', 'B', 'C', 'D'],
+                    labels: ['1', '2', '3', '4'],
                     datasets: [
                         {
-                            label: ['A', 'B', 'C', 'D'],
+                            label: ['1', '2', '3', '4'],
                             backgroundColor: ['#5f9ea0', '#eee8aa', '#ffa07a', '#dda0dd'],
                             data: [this.counterA, this.counterB, this.counterC, this.counterD]
                             // data: [ method1(), method2(), method3(), method4() ]
                         }
                     ]
                 }
-            },
-
-            // チャートを初期化する
-            reset: function () {
-                alert("RESET")
-                // ボタンを押したら、投票を無効にしてラジオボタンを初期化
-                this.counterA = 1
-                this.counterB = 1
-                this.counterC = 1
-                this.counterD = 1
-                // this.form.submit()
-            },
-
-            // 定期実行
-            reDraw:function () {
-                this.fillData().interval(10000)
             }
         }
     }
@@ -123,25 +95,6 @@
     .small {
         max-width: 400px;
         margin:  25px auto;
-    }
-
-    .wrapper{
-        height: 100%;
-        min-height: 100vh;
-        position: relative;/*←相対位置*/
-        padding-bottom: 100px;/*←footerの高さ*/
-        box-sizing: border-box;/*←全て含めてmin-height:100vhに*/
-    }
-
-    footer{
-        width: 100%;
-        background-color: #87cefa;
-        color: #fff;
-        text-align: center;
-        padding: 10px 0;
-
-        position: absolute;/*←絶対位置*/
-        bottom: 0; /*下に固定*/
     }
 
 </style>
